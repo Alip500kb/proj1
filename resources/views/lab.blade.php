a8971d125eaf946d02fa3767080e66d4e67b4218
Alipp <alifbintangjayaki@gmail.com>
2026-02-11 23:57:06 +0700

    besok aj

---
+++ resources/views/lab.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lab</title>
+</head>
+<body>
+    <a href="/docs">Documentation</a>
+    <a href="/dashboard">Dasboard</a>
+    <a href="/lab">Lab</a>
+    <br>
+    <h1>welcome to our laravel project lab page</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/docs', function () {
+    return view('docs');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+Route::get('/lab', function () {
+    return view('lab');
+});
